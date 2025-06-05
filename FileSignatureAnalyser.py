79dd9b79b5a8cccfbadeadd3554e29012a559500
Alfin Jerome <128155657+alfinjerome@users.noreply.github.com>
2025-06-05 22:39:38 +0530

    Update FileSignatureAnalyser.py
    Print the byte stream in 16 bits wide

--- FileSignatureAnalyser.py
+++ FileSignatureAnalyser.py
@@ -1,12 +1,17 @@
 import binascii
 import re
 
-file = input("Enter file name:")
+file = r'X:\Phone\Images\DataUsage.jpg' #input("Enter file name:")
 
 with open(file,'rb') as analyse:
     content=analyse.read()
 
-print(content)  
+#print(content)  
 
-hexsignature = binascii.hexlify(content,' ')
-print(hexsignature)
+hexsignature = binascii.hexlify(content,' ').upper().decode()
+
+#print(hexsignature)
+
+with open('X:\\text.txt','w') as out:
+    for i in range(0, len(hexsignature),48):
+            out.write(hexsignature[i:i+48]+'\n')
